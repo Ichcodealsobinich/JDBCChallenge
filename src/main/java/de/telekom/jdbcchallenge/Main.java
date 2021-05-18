7c33739e8e6cc4e45528cc7c9cbc7dd40ffdb0ce
sea4 <f.leonhardt@telekom.de>
2021-05-18 15:56:30 +0200

    Used PreparedStatement to prevent SQL-Injections

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -2,39 +2,42 @@ package de.telekom.jdbcchallenge;
 
 import java.sql.*;
 
-
 public class Main {
-
 	public static void main(String[] args) {	
 		/* Load driver // throws ClassNotFoundException*/
 		try {
 			final String DRIVER = "org.mariadb.jdbc.Driver";
 			Class.forName(DRIVER);
-		} catch (ClassNotFoundException e) {}
+		} catch (Exception e) {e.printStackTrace();}
 		
 		/* Establish connection using DriverManager */
 		try (Connection connection = DriverManager
 				.getConnection("jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass");){			
 				
-			/*create the statement and execute it*/
-			try (Statement statement = connection.createStatement();){
+			/*create a preparedStatement for writing and execute it*/
+			String sql = "INSERT INTO personen ( ID, ANREDE, VORNAME, NACHNAME) VALUES ( ?, ?, ?, ? )";
+			try (PreparedStatement ps = connection.prepareStatement(sql);){
+				ps.setString(1, "1");
+				ps.setString(2, "1");
+				ps.setString(3, "Paul");
+				ps.setString(4, "Paulsen");
+				ps.executeQuery();
 				
-				String  query = "INSERT INTO personen "
-					+ "(id, anrede, vorname, nachname) "
-					+ "VALUES (3,0,'Paula', 'Paulsen')";
-				statement.executeQuery(query);
-				query = "SELECT * FROM personen";				
-				try (ResultSet resultSet = statement.executeQuery(query);) {			
-				
-					/*iterate through the resultSet and print the data*/
-					while (resultSet.next())  {
-						System.out.println(resultSet.getString(1));
-						System.out.println(resultSet.getString(2));
-						System.out.println(resultSet.getString(3));
-						System.out.println(resultSet.getString(4));
-					}
-				} catch (Exception e) {	}//resultSet.close() will be called automatically
-			}catch (Exception e) { } //statement.close() will be called automatically					
+				/*create a "normal" statement for reading*/
+				String query= "SELECT * FROM personen";					
+				try(Statement statement = connection.createStatement()) {
+					try (ResultSet resultSet = statement.executeQuery(query);) {			
+						
+						/*iterate through the resultSet and print the data*/
+						while (resultSet.next())  {
+							System.out.println(resultSet.getLong(1));
+							System.out.println(resultSet.getShort(2));
+							System.out.println(resultSet.getString(3));
+							System.out.println(resultSet.getString(4));
+						}
+					} catch (Exception e) {	} //resultSet.close() will be called automatically				
+				}catch (Exception e) { } //statement.close() will be called automatically
+			}catch (Exception e) {e.printStackTrace();} //preparedStatement.close() will be called automatically
 		} catch (Exception e) {	} //connection.close() will be called automatically
 	}
 }
