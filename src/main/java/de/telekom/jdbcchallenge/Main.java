caea3a06f840edc8bbae4708a1fa039bd697089b
sea4 <f.leonhardt@telekom.de>
2021-05-18 11:43:58 +0200

    Load driver

---
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -0,0 +1,21 @@
+package de.telekom.jdbcchallenge;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class Main {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		final String DRIVER = "org.mariadb.jdbc.Driver";
+		Class.forName(DRIVER);
+		
+		try {
+				Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass");
+		} catch (Exception e) {}
+		
+
+
+	}
+
+}
